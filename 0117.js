82ac96fd8ebed7a52b09454f5d742d0dd70c8c30
GYUWON <37896874+GyuWonLee@users.noreply.github.com>
2023-01-17 07:30:19 +0900

    Create 0117.js

---
+++ 0117.js
@@ -0,0 +1,11 @@
+let age = 25; // 정수
+let tall = 175.9; // 실수
+
+console.log(age * tall);
+
+let inf = Infinity; // 숫자로 정의가 되어있음 무한대
+let minusInf = -Infinity; // 음의 무한대
+
+let nan = NaN; // 연산이 잘못 수행되었을 때 수학적 계산의 실패
+
+console.log(age * tall);
