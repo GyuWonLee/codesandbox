e1fed936db487fcf1e5df195ea518323171cc177
GYUWON <37896874+GyuWonLee@users.noreply.github.com>
2023-01-24 22:15:08 +0900

    Create findIndex.js

---
+++ src/findIndex.js
@@ -0,0 +1,10 @@
+const arr = [
+  { color: "red" },
+  { color: "black" },
+  { color: "blue" },
+  { color: "green" }
+];
+
+let number = 3;
+
+console.log(arr.findIndex((elm) => elm.color === "green"));
