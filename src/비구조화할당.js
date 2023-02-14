72d0f2dc85b7db9d362148bfe796041aa155bda7
GYUWON <37896874+GyuWonLee@users.noreply.github.com>
2023-02-14 17:08:43 +0900

    Create 비구조화할당.js

---
+++ src/비구조화할당.js
@@ -0,0 +1,14 @@
+// 배열의 선언 분리
+// 비구조화 할당
+let [one, two, three, four = "four"] = ["one", "two", "three"];
+console.log(one, two, three, four);
+
+// 배열의 비구조화 할당
+//let [one, two, three] = arr;
+//console.log(one, two, three);
+
+// let one = arr[0];
+// let two = arr[1];
+// let three = arr[2];
+
+// console.log(one, two, three);
